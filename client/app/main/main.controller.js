1bd5926bacc128fe784737fa2b7e4cd95baece51
Zoe Landon <zoe@leporidaemedia.com>
2014-08-09 22:01:32 -0700

    missed file

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('rcrdboxApp')
+  .controller('MainCtrl', function ($scope, $http, stripe) {
+    
+    function GetQueryStringParams(sParam) {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++)
+        {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam)
+            {
+                return sParameterName[1];
+            }
+        }
+    };
+    
+    $scope.checkout = function(plan) {
+      stripe.checkout(plan);
+      event.preventDefault();
+    };
+
+  });
